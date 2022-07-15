48ad677a401cb1a50a93ad46dbe82bcc89b26406
Tanya Verma <tanyaverma3114@gmail.com>
2022-07-16 01:24:44 +0530

    Selection Sort

---
+++ SelectionSort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+// #include <algorithm>
+#include <vector>
+using namespace std;
+void SelectionSort(vector<int> arr, int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+    {
+        int min = i;
+        for (j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min])
+                min = j;
+            swap(arr[i], arr[min]);
+        }
+        
+    }
+    for (int x : arr)
+    {
+        cout << x << " ";
+    }
+}
+int main()
+{
+    vector<int> arr;
+    int n, a, i;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    cout << "Enter the elements of the array: ";
+    for (i = 0; i < n; i++)
+    {
+        cin >> a;
+        arr.push_back(a);
+    }
+    SelectionSort(arr, n);
+    return 0;
+}
