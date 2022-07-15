a87a2e465a465807af65568c2e7fb5da0210c1d0
Tanya Verma <tanyaverma3114@gmail.com>
2022-07-16 02:10:50 +0530

    Bubble Sort

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -8,7 +8,7 @@ void BubbleSort(vector<int> arr, int n)
     {
         for (j = 0; j < n - i - 1; j++)
         {
-            if (arr[j] < arr[j + 1])
+            if (arr[j] > arr[j + 1])
                 swap(arr[j], arr[j + 1]);
         }
     }
