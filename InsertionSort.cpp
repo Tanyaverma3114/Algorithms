d8a3d3a402d780569f0940849e13fe7ba3b2872f
Tanya Verma <tanyaverma3114@gmail.com>
2022-12-21 02:39:14 +0530

    Insertion Sort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -4,8 +4,18 @@ using namespace std;
 void InsertionSort(vector<int> arr, int n)
 {
     int i, j;
-   
-    for (int x : arr)
+    for (i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+    for (auto x : arr)
     {
         cout << x << " ";
     }
